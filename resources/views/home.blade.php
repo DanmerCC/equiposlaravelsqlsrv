24e47282ca87b1af8f07d908e872f23f380b4985
Danmer Ccoscco <danmerccosccogmail.com>
2022-11-16 23:19:49 -0500

    corrando asesores en dsaboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,19 +44,7 @@
             <li class="breadcrumb-item active">Contador de equipos asignados</li>
         </ol>
         <div class="row">
-            @for ($i = 0; $i < count($resumegrups); $i++)
-                <div class="col-xl-3 col-md-6">
-                    <div @click="$refs.equipostable.toggleGruposFilter('{{ $resumegrups[$i]->asesor->equipo->dni }}')"
-                        class="card {{ $i % 2 == 0 ? 'bg-primary' : 'bg-danger' }}  text-white mb-4">
-                        <div class="card-body"> {{ $resumegrups[$i]->asesor->equipo->nombres }} :</div>
-                        <div class="card-footer d-flex align-items-center justify-content-between">
-                            <h3>{{ $resumegrups[$i]->total }} Laptops<sup style="font-size: 20px"></sup></h3>
-                            <!--<a class="small text-white stretched-link" href="Tabla_CallCenter.php">></a>
-                                                                                                                                                <div class="small text-white"><i class="fas fa-angle-right"></i></div>-->
-                        </div>
-                    </div>
-                </div>
-            @endfor
+
             <div class="col-xl-3 col-md-6">
                 <div class="card {{ $i % 2 == 0 ? 'bg-primary' : 'bg-success' }}  text-white mb-4">
                     <div class="card-body"> No Asignados</div>
